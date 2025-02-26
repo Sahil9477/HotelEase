5467baaecd2a3eff9517327f23d6e96e42bdf174
sahil_bhatt <sahilbhatt1602@gmail.com>
2025-02-25 21:35:35 -0800

    first commit

---
+++ src/test/java/com/example/HotelEase/HotelEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
